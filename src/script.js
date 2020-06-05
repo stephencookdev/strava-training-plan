e8c176816dec49e7e1c6e10983308581f11efe2b
Stephen Cook <stephen@stephencookdev.co.uk>
2020-06-06 00:48:21 +0100

    Get some actual training suggestions in, with hardcoded values

--- src/script.js
+++ src/script.js
@@ -1,5 +1,43 @@
 const target = document.getElementById("target");
 
+const RACE_WEIGHT = 100;
+const DAYS_TO_ZERO_WEIGHTING = 7 * 6; // (6 weeks)
+const TAPER_REDUCTION = 0.65;
+
+const REGION = "en-GB";
+
+const MINUTE_IN_MS = 1000 * 60;
+const DAY_IN_MS = MINUTE_IN_MS * 60 * 24;
+const WEEK_IN_MS = DAY_IN_MS * 7;
+
+const TARGET_RACE = {
+  distance: 42195,
+  movingTime: 14400,
+  trainingStartDate: new Date("2020-05-11"),
+  date: new Date("2020-09-13Z09:00"),
+  taper: 18 * DAY_IN_MS,
+};
+
+const TRAINING_PREFS = {
+  0: null, // Monday
+  1: "speed", // Tuesday
+  2: "recovery", // Wednesday
+  3: null, // Thursday
+  4: "recovery", // Friday,
+  5: null, // Saturday
+  6: "long", // Sunday
+};
+
+const Riegel = {
+  R: 1.15,
+  getNewTime(newDistance, old) {
+    return old.movingTime * Math.pow(newDistance / old.distance, Riegel.R);
+  },
+  getNewDistance(newTime, old) {
+    return old.distance * Math.pow(newTime / old.movingTime, 1 / Riegel.R);
+  },
+};
+
 const grabAccessTokens = async () => {
   const queryParams = new URLSearchParams(location.search);
   const authCode = queryParams.get("code");
@@ -20,9 +58,13 @@ const grabAccessTokens = async () => {
   }
 };
 
-const getActivities = async (accessToken) => {
+const getActivities = async (accessToken, { before, after } = {}) => {
+  const queryParams = [];
+  if (before) queryParams.push(`before=${Math.floor(before / 1000)}`);
+  if (after) queryParams.push(`after=${Math.floor(after / 1000)}`);
+
   const listResponse = await fetch(
-    "https://www.strava.com/api/v3/athlete/activities",
+    `https://www.strava.com/api/v3/athlete/activities?${queryParams.join("&")}`,
     {
       headers: {
         Authorization: `Bearer ${accessToken}`,
@@ -33,7 +75,7 @@ const getActivities = async (accessToken) => {
 
   return list.map((activity) => {
     return {
-      date: activity.start_date,
+      date: new Date(activity.start_date),
       distance: activity.distance,
       movingTime: activity.moving_time,
       isRace: activity.workout_type === 1,
@@ -41,14 +83,351 @@ const getActivities = async (accessToken) => {
   });
 };
 
+const getTimeWeighting = (activity) => {
+  const date = activity.date;
+  const ZERO_THRESHOLD = DAY_IN_MS * DAYS_TO_ZERO_WEIGHTING;
+  const timeOfZero = TARGET_RACE.trainingStartDate - ZERO_THRESHOLD;
+
+  const t = Math.max(0, Math.min(1, (date - timeOfZero) / ZERO_THRESHOLD));
+
+  return t * t;
+};
+
+const getRaceWeighting = (activity) => {
+  return activity.isRace ? RACE_WEIGHT : 1;
+};
+
+const getCurrentPotential = (activities) => {
+  const cookedActivities = activities.map((activity) => ({
+    ...activity,
+    weight: getTimeWeighting(activity) * getRaceWeighting(activity),
+  }));
+
+  const summedActivity = cookedActivities.reduce(
+    (acc, activity) => ({
+      distance: acc.distance + activity.distance * activity.weight,
+      movingTime: acc.movingTime + activity.movingTime * activity.weight,
+      weight: acc.weight + activity.weight,
+    }),
+    { distance: 0, movingTime: 0, weight: 0 }
+  );
+
+  const averageActivity = {
+    distance: summedActivity.distance / summedActivity.weight,
+    movingTime: summedActivity.movingTime / summedActivity.weight,
+  };
+
+  const potential = {
+    distance: averageActivity.distance,
+    movingTime: averageActivity.movingTime,
+    pace: averageActivity.movingTime / averageActivity.distance,
+    weeklyMileage: getWeeklyMileage(activities),
+  };
+
+  return potential;
+};
+
+const getWeeklyMileage = (activities) => {
+  const sortedActivities = activities.sort((a, b) => a.date - b.date);
+  const earliestActivity = sortedActivities[0];
+  const latestActivity = sortedActivities[sortedActivities.length - 1];
+
+  const ONE_DAY = 1000 * 60 * 60 * 24;
+  const daysBetween = Math.ceil(
+    (latestActivity.date - earliestActivity.date) / ONE_DAY
+  );
+  let dayWeightingSum = 0;
+  for (let i = 0; i <= daysBetween; i++) {
+    dayWeightingSum += getTimeWeighting({
+      date: TARGET_RACE.trainingStartDate - ONE_DAY * i,
+    });
+  }
+
+  const summedMileage = activities.reduce(
+    (acc, cur) => acc + cur.distance * getTimeWeighting(cur),
+    0
+  );
+
+  return (7 * summedMileage) / dayWeightingSum;
+};
+
+const humanDistance = (distance) => {
+  const distanceKm = distance / 1000;
+  return `${distanceKm.toFixed(1)}km`;
+};
+
+const humanPace = (movingTime, distance) => {
+  const secondMeterPace = movingTime / distance;
+  const minuteKmPace = (1000 * secondMeterPace) / 60;
+
+  const minuteKmPaceWhole = Math.floor(minuteKmPace);
+  const minuteKmPaceSeconds = Math.floor(
+    (minuteKmPace - minuteKmPaceWhole) * 60
+  )
+    .toString()
+    .padStart(2, "0");
+
+  const cookedMinuteKmPace = `${minuteKmPaceWhole}:${minuteKmPaceSeconds}`;
+
+  return cookedMinuteKmPace;
+};
+
+const getPeakReqs = (target) => {
+  const peakDistance = 2 * target.distance;
+
+  const targetSecondMeterPace = TARGET_RACE.movingTime / TARGET_RACE.distance;
+  const targetMinuteKmPace = (1000 * targetSecondMeterPace) / 60;
+  const peakMinuteKmPace = targetMinuteKmPace + 0.4;
+  const peakSecondMeterPace = (60 * peakMinuteKmPace) / 1000;
+
+  return { distance: peakDistance, pace: peakSecondMeterPace };
+};
+
+const getStartOfDay = (date) => {
+  return Math.floor(date / DAY_IN_MS) * DAY_IN_MS + 1;
+};
+
+const getEndOfDay = (date) => {
+  return Math.ceil(date / DAY_IN_MS) * DAY_IN_MS - 1;
+};
+
+const getRawWeeks = (target, targetPeak, distanceInc, speedInc) => {
+  // normalise our INC vars, we shouldn't ever be decreasing things
+  distanceInc = Math.max(1, distanceInc);
+  speedInc = Math.max(1, speedInc);
+
+  const numberOfDays = Math.ceil(
+    (target.date - target.trainingStartDate) / DAY_IN_MS
+  );
+  const numberOfWeeks = Math.floor(numberOfDays / 7);
+
+  const weeks = [];
+  for (let i = 0; i < numberOfWeeks; i++) {
+    const power = numberOfWeeks - i - 1;
+
+    weeks[i] = {
+      distance: targetPeak.distance * Math.pow(1 / distanceInc, power),
+      pace: targetPeak.pace * Math.pow(speedInc, power),
+      weekStart:
+        getStartOfDay(target.date - WEEK_IN_MS * (numberOfWeeks - i)) +
+        DAY_IN_MS,
+      weekEnd: getEndOfDay(target.date - WEEK_IN_MS * (numberOfWeeks - i - 1)),
+    };
+  }
+
+  return weeks;
+};
+
+const taperWeeks = (weeks, taper) => {
+  const dateOfTaper = TARGET_RACE.date - taper;
+
+  return weeks.map((week, i) => {
+    const taperRatio = Math.max(
+      0,
+      Math.min(
+        1,
+        (week.weekEnd - dateOfTaper) / (week.weekEnd - week.weekStart)
+      )
+    );
+    const taperAmount = 1 - taperRatio * TAPER_REDUCTION;
+
+    return {
+      ...week,
+      distance: week.distance * taperAmount,
+    };
+  });
+};
+
+const interleaveRestWeeks = (weeks) => {
+  const totalMileage = weeks.reduce((acc, cur) => acc + cur.distance, 0);
+
+  const rawRestedWeeks = weeks.map((week, i) => {
+    // ideal interleaving is NORMAL / NORMAL / TAPER
+    // we taper at the end, so make sure to end on an TAPER
+    const shouldRest = (i - weeks.length + 1) % 3 === 0;
+
+    return {
+      ...week,
+      distance: week.distance * (shouldRest ? 0.8 : 1),
+    };
+  });
+
+  const rawRestedMileage = rawRestedWeeks.reduce(
+    (acc, cur) => acc + cur.distance,
+    0
+  );
+
+  const adjustment = totalMileage / rawRestedMileage; // adjust so we still do the same overall mileage
+
+  const restedWeeks = rawRestedWeeks.map((week) => ({
+    ...week,
+    distance: week.distance * adjustment,
+  }));
+
+  return restedWeeks;
+};
+
+const addMovingTimeToWeeks = (weeks) =>
+  weeks.map((week) => ({
+    ...week,
+    movingTime: week.pace * week.distance,
+  }));
+
+const weekActivitiesSoFar = (activitiesOfWeek) => {
+  return activitiesOfWeek
+    .map(
+      (activity) =>
+        ` - ${humanDistance(activity.distance)} @ ${humanPace(
+          activity.movingTime,
+          activity.distance
+        )}`
+    )
+    .join("\n");
+};
+
+const weekPlanPast = (activitiesOfWeek, totalActivities) => {
+  const activitiesString = weekActivitiesSoFar(activitiesOfWeek);
+
+  return (
+    activitiesString +
+    "\n" +
+    `(total ${humanDistance(totalActivities.distance)} @ ${humanPace(
+      totalActivities.movingTime,
+      totalActivities.distance
+    )})`
+  );
+};
+
+const weekPlanCurrent = (week, activitiesOfWeek, totalActivities) => {
+  const remaining = {
+    distance: Math.max(0, week.distance - totalActivities.distance),
+    movingTime: Math.max(0, week.movingTime - totalActivities.movingTime),
+  };
+
+  const recommendedRemaining = `Recommended this week: ${humanDistance(
+    remaining.distance
+  )} @ ${humanPace(remaining.movingTime, remaining.distance)}`;
+
+  return weekActivitiesSoFar(activitiesOfWeek) + "\n" + recommendedRemaining;
+};
+
+const weekPlanFuture = (week, activitiesOfWeek, totalActivities) => {
+  return weekPlanCurrent(week, activitiesOfWeek, totalActivities);
+};
+
+const weekPlanString = (week, sinceTrainingPlanActivities) => {
+  const start = new Date(week.weekStart);
+  const end = new Date(week.weekEnd);
+
+  const activitiesOfWeek = sinceTrainingPlanActivities.filter(
+    (activity) => week.weekStart < activity.date && activity.date < week.weekEnd
+  );
+
+  const totalActivities = activitiesOfWeek.reduce(
+    (acc, cur) => ({
+      distance: acc.distance + cur.distance,
+      movingTime: acc.movingTime + cur.movingTime,
+    }),
+    { distance: 0, movingTime: 0 }
+  );
+
+  if (end < Date.now()) {
+    return weekPlanPast(activitiesOfWeek, totalActivities);
+  } else if (start > Date.now()) {
+    return weekPlanFuture(week, activitiesOfWeek, totalActivities);
+  } else {
+    return weekPlanCurrent(week, activitiesOfWeek, totalActivities);
+  }
+};
+
 const main = async () => {
   await grabAccessTokens();
 
   if (localStorage.getItem("accessToken")) {
     const accessToken = localStorage.getItem("accessToken");
-    const activities = await getActivities(accessToken);
+    const historicalActivities = await getActivities(accessToken, {
+      before: TARGET_RACE.trainingStartDate,
+    });
+    const sinceTrainingPlanActivities = await getActivities(accessToken, {
+      after: TARGET_RACE.trainingStartDate,
+    });
 
-    target.innerHTML = JSON.stringify(activities, null, 2);
+    const potential = getCurrentPotential(historicalActivities);
+
+    const riegelRacePrediction = {
+      timeAtRaceDistance: Riegel.getNewTime(TARGET_RACE.distance, potential),
+      distanceAtRacePace: Riegel.getNewDistance(
+        TARGET_RACE.movingTime,
+        potential
+      ),
+    };
+
+    const targetPeak = getPeakReqs(TARGET_RACE);
+    const msUntilRaceFromTrainingStart =
+      TARGET_RACE.date - TARGET_RACE.trainingStartDate;
+
+    const distanceInc = Math.pow(
+      targetPeak.distance / potential.weeklyMileage,
+      WEEK_IN_MS / msUntilRaceFromTrainingStart
+    );
+    const speedInc =
+      1 /
+      Math.pow(
+        targetPeak.pace / potential.pace,
+        WEEK_IN_MS / msUntilRaceFromTrainingStart
+      );
+
+    const rawWeeks = getRawWeeks(
+      TARGET_RACE,
+      targetPeak,
+      distanceInc,
+      speedInc
+    );
+    const weeks = addMovingTimeToWeeks(
+      taperWeeks(interleaveRestWeeks(rawWeeks), TARGET_RACE.taper)
+    );
+
+    target.innerHTML = `
+      Potential: distance ${humanDistance(potential.distance)} / ${humanPace(
+      potential.movingTime,
+      potential.distance
+    )} pace
+      Weekly mileage: ${humanDistance(potential.weeklyMileage)}
+      
+      Target pace: ${humanPace(TARGET_RACE.movingTime, TARGET_RACE.distance)}
+      Target distance: ${humanDistance(TARGET_RACE.distance)}
+      Riegel pace @ race distance: ${humanPace(
+        riegelRacePrediction.timeAtRaceDistance,
+        TARGET_RACE.distance
+      )}
+      Riegel distance @ race pace: ${humanDistance(
+        riegelRacePrediction.distanceAtRacePace
+      )}
+
+      Distance INC: ${distanceInc.toFixed(2)}
+      Speed INC: ${speedInc.toFixed(2)}
+
+      Weeks:
+${weeks
+  .map((week) => {
+    const start = new Date(week.weekStart);
+    const dateF = new Intl.DateTimeFormat(REGION, {
+      weekday: "short",
+      month: "short",
+      day: "numeric",
+    });
+
+    const weekSummary = `Week of ${dateF.format(start)} — ${humanDistance(
+      week.distance
+    )} / ${humanPace(week.pace, 1)}`;
+
+    return (
+      weekSummary + "\n" + weekPlanString(week, sinceTrainingPlanActivities)
+    );
+  })
+  .join("\n\n")}
+    `;
+
     target.style = "white-space: pre;";
   } else {
     const button = document.createElement("a");
