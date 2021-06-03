4488c68045abb19e5bedb0a2592e3f1a75e27695
Stephen Cook <stephen@stephencookdev.co.uk>
2021-06-03 22:44:02 +0100

    Add support for refresh-token usage in the auth API

--- lambda_functions/auth.js
+++ lambda_functions/auth.js
@@ -1,13 +1,13 @@
 const axios = require("axios");
 
 module.exports.handler = async (event, context) => {
-  const authCode = event.body;
+  const { authCode, refreshToken } = event.body;
 
   const authResponse = await axios.post("https://www.strava.com/oauth/token", {
     client_id: process.env.CLIENT_ID,
     client_secret: process.env.CLIENT_SECRET,
-    code: authCode,
-    grant_type: "authorization_code",
+    grant_type: authCode ? "authorization_code" : "refresh_token",
+    ...(authCode ? { code: authCode } : { refresh_token: refreshToken }),
   });
 
   return {
@@ -18,6 +18,7 @@ module.exports.handler = async (event, context) => {
     body: JSON.stringify({
       accessToken: authResponse.data.access_token,
       refreshToken: authResponse.data.refresh_token,
+      accessTokenExpiresAt: authResponse.data.expires_at,
     }),
   };
 };
